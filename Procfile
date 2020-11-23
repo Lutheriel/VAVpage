b864332c9e0bdaa9c47d86fde33a1807a522bfef
Petra Romanová <petra.romanova@student.tuke.sk>
2020-11-23 19:28:12 +0100

    heroku test

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
+web: strapi start
+web: cd vue
 web: npm run serve
--- vue/src/main.js
+++ vue/src/main.js
@@ -8,7 +8,6 @@ import "./assets/css/bootstrap.css";
 Vue.config.productionTip = true;
 Vue.use(VueAxios, axios);
 axios.defaults.baseURL = process.env.VUE_APP_API;
-debugger
 
 new Vue({
   render: (h) => h(App),
