095d4823074ac1c4eaa3efb250bf64c740b4435c
Petra Romanová <petra.romanova@student.tuke.sk>
2020-11-28 23:29:33 +0100

    commented router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="app">
-    <div class="routing">
-    </div>
-    <router-view/>
+    <!-- <div class="routing"> -->
+    <Home />
+    <!-- </div> -->
+    <!-- <router-view/> -->
   </div>
 </template>
 
 <script>
+import Home from './components/Home.vue'
 
 export default {
-  name: 'App',
-  methods: {
-    loadHome: function(){
-      this.$router.push('/home').catch(() => {})
-    }
-  },
-  mounted: function(){
-    this.loadHome()
-  }
+  components: { Home },
+  name: 'App'
+  // methods: {
+  //   loadHome: function(){
+  //     this.$router.push('/home').catch(() => {})
+  //   }
+  // },
+  // mounted: function(){
+  //   this.loadHome()
+  // }
 }
 </script>
--- src/router.js
+++ src/router.js
@@ -1,24 +1,24 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import App from './App.vue';
-// import Photo from "./views/PhotoView";
-import Home from "./components/Home";
+// import Vue from 'vue';
+// import Router from 'vue-router';
+// import App from './App.vue';
+// // import Photo from "./views/PhotoView";
+// import Home from "./components/Home";
 
-Vue.use(Router);
+// Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'app',
-      component: App,
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: Home,
-    }
-  ],
-});
+// export default new Router({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'app',
+//       component: App,
+//     },
+//     {
+//       path: '/home',
+//       name: 'home',
+//       component: Home,
+//     }
+//   ],
+// });
