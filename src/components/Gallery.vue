856ca6545beb476017cdb77af93a87f39eaaa617
Petra Romanová <petra.romanova@student.tuke.sk>
2020-11-28 22:35:13 +0100

    minor changes

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="bg-light" id="gallery">
   <h2 class="section-heading text-uppercase">Gallery</h2>
+            <h3 class="section-subheading text-muted"></h3>
     <img class="image" v-for="(image, i) in images" :src="preUrl + image.url" @click="index = i" :key="image.id">
   <vue-gallery-slideshow :images="imagesUrl" :index="index" @close="index = null"></vue-gallery-slideshow>
   </section>
@@ -41,24 +42,4 @@ export default {
   border: 1px solid lightgray;
   object-fit: contain;
 }
-.gallery {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
-    grid-gap: 1rem;
-    max-width: 80rem;
-    margin: 5rem auto;
-    padding: 0 5rem;
-  }
-.gallery-panel img {
-    width: 100%;
-    height: 22vw;
-    object-fit: cover;
-    border-radius: 0.75rem;
-  }
-h2{
-  text-align: center;
-  font-size: 2rem;
-  margin-bottom: 15px;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}  
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -39,7 +39,4 @@ export default {
 </script>
 
 <style lang="css">
-/* .backgroundImage{
-  background-image:url(../assets/img/header-bg-copy.jpg) !important
-} */
 </style>
